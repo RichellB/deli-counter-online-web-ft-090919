25cbe79704470495f40a52f3bf03843f1581a631
Richell Bonfield <richell.bonfield12@gmail.com>
2019-09-14 17:23:43 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -23,8 +23,9 @@ def now_serving(array)
     puts "There is nobody waiting to be served!"
   else 
     puts "Currently serving #{array.first}."
+    array.shift
   end
-  array.shift
+  
 end
     
     
