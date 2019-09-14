ba7c85f1cfc937aae75f179aea485212de68b958
Richell Bonfield <richell.bonfield12@gmail.com>
2019-09-14 17:07:30 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -6,9 +6,10 @@ def line(katz_deli)
     puts "The line is currently empty."
   else
     katz_deli.each_with_index do |name, position|
-      "The line is currently: #{position+1}."
+      line_position = "The line is currently: #{position} #{name}."
     end
   end
+    line_position
 end
   
 def take_a_number(katz_deli, name)
