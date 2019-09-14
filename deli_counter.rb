9d8bf639ec62b3ef3b701e756fdb9a85e97a995f
Richell Bonfield <richell.bonfield12@gmail.com>
2019-09-14 17:21:57 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -18,8 +18,8 @@ def take_a_number(array, name)
   puts "Welcome, #{name}. You are number #{array.length} in line."
 end
 
-def now_serving
-  if katz_deli.empty?
+def now_serving(array)
+  if array.empty?
     puts "There is nobody waiting to be served!"
   else 
     puts "Currently serving #{katz_deli.first}."
